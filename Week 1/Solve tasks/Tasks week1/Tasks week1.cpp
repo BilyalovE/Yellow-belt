27d2656e4b586f8e38dd46a9494695314609d35c
eldar <eldar@ASER_ASPIRE_7E>
2024-08-04 13:28:41 +0300

    save

--- Week 1/Solve tasks/Tasks week1/Tasks week1.cpp
+++ Week 1/Solve tasks/Tasks week1/Tasks week1.cpp
@@ -724,3 +724,33 @@ TEST(Week_1, Task_6) {
 		cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
 	}
 }
+
+/*
+Задание по программированию: Ссылка на элемент
+Реализуйте шаблонную функцию GetRefStrict, которая на вход принимает: map и значение ключа k.
+Если элемент по ключу k в коллекции отсутствует, то функция должна бросить исключение runtime_error, 
+иначе вернуть ссылку на элемент в коллекции.
+
+Пример использования функции*/
+template<typename KeyType, typename ValueType>
+ValueType& GetRefStrict(map<KeyType, ValueType>& input_map, const KeyType key) {
+	if (input_map.count(key) == 0) {
+		throw runtime_error("no such key in dictionary");
+	}
+	return input_map.at(key);
+}
+
+
+
+TEST(Week_1, Task_7) {
+	map<int, string> m = { {0, "value"} };
+	try {
+		string& item = GetRefStrict(m, 0);
+		item = "newvalue";
+		cout << m[0] << endl; // выведет newvalue
+	}
+	catch (exception& ex){
+		cout << ex.what() << endl;
+	}
+	
+}
